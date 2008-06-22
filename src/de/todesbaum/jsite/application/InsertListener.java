bb52f8a8424a1fb38c99875dc17b39648b2d5d8b
David ‘Bombe’ Roden <bombe@freenetproject.org>
2008-06-22 11:25:00 +0200

    add javadoc

--- src/de/todesbaum/jsite/application/InsertListener.java
+++ src/de/todesbaum/jsite/application/InsertListener.java
@@ -22,19 +22,83 @@ package de.todesbaum.jsite.application;
 import java.util.EventListener;
 
 /**
+ * Interface for objects that want to be notified abount insert events.
+ * 
  * @author David ‘Bombe’ Roden &lt;bombe@freenetproject.org&gt;
  */
 public interface InsertListener extends EventListener {
 
+	/**
+	 * Enumeration for the different error situations.
+	 * 
+	 * @author David ‘Bombe’ Roden &lt;bombe@freenetproject.org&gt;
+	 */
 	public static enum ErrorType {
-		KEY_COLLISION, ROUTE_NOT_FOUND, DATA_NOT_FOUND, FCP_ERROR, IO_ERROR
+
+		/** The key does already exist. */
+		KEY_COLLISION,
+
+		/** The route to the key was not found. */
+		ROUTE_NOT_FOUND,
+
+		/** The data was not found. */
+		DATA_NOT_FOUND,
+
+		/** Error in the FCP communication. */
+		FCP_ERROR,
+
+		/** General error in the communication. */
+		IO_ERROR
 	}
 
+	/**
+	 * Notifies a listener that an insert has started.
+	 * 
+	 * @param project
+	 *            The project that is now being inserted
+	 */
 	public void projectInsertStarted(Project project);
+
+	/**
+	 * Notifies a listener that a project insert has generated a URI.
+	 * 
+	 * @param project
+	 *            The project being inserted
+	 * @param uri
+	 *            The generated URI
+	 */
 	public void projectURIGenerated(Project project, String uri);
 
+	/**
+	 * Notifies a listener that an insert has made some progress.
+	 * 
+	 * @param project
+	 *            The project being inserted
+	 * @param succeeded
+	 *            The number of succeeded blocks
+	 * @param failed
+	 *            The number of failed blocks
+	 * @param fatal
+	 *            The number of fatally failed blocks
+	 * @param total
+	 *            The total number of blocks
+	 * @param finalized
+	 *            <code>true</code> if the total number of blocks has been
+	 *            finalized, <code>false</code> otherwise
+	 */
 	public void projectInsertProgress(Project project, int succeeded, int failed, int fatal, int total, boolean finalized);
 
+	/**
+	 * Notifies a listener that a project insert has finished.
+	 * 
+	 * @param project
+	 *            The project being inserted
+	 * @param success
+	 *            <code>true</code> if the insert succeeded,
+	 *            <code>false</code> otherwise
+	 * @param cause
+	 *            The cause of a failure, if any (may be <code>null</code>)
+	 */
 	public void projectInsertFinished(Project project, boolean success, Throwable cause);
 
 }
