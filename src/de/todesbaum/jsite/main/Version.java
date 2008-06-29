921168a43ff2ec536bb111d37212f1332c54ca5d
David ‘Bombe’ Roden <bombe@freenetproject.org>
2008-06-29 19:25:50 +0200

    increase version to 0.5

--- src/de/todesbaum/jsite/main/Version.java
+++ src/de/todesbaum/jsite/main/Version.java
@@ -27,7 +27,7 @@ package de.todesbaum.jsite.main;
 public class Version {
 
 	/** The version. */
-	private static final String VERSION = "0.4.12.2";
+	private static final String VERSION = "0.5";
 
 	/**
 	 * Returns the version.
