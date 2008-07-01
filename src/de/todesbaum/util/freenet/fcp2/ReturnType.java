fcd5d843f209af2e9fe2dc9e86e2ef2bfd103a1a
David ‘Bombe’ Roden <bombe@freenetproject.org>
2008-07-01 08:43:19 +0200

    add return type for ClientGet

---
+++ src/de/todesbaum/util/freenet/fcp2/ReturnType.java
@@ -0,0 +1,39 @@
+/*
+ * jSite-remote - ReturnType.java -
+ * Copyright © 2008 David Roden
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+package de.todesbaum.util.freenet.fcp2;
+
+/**
+ * Enumeration for the different return types a {@link ClientGet} request can
+ * have.
+ *
+ * @author David ‘Bombe’ Roden &lt;bombe@freenetproject.org&gt;
+ */
+public enum ReturnType {
+
+	/** The data is returned as payload. */
+	direct,
+
+	/** The data is written to disk. */
+	disk,
+
+	/** The data is not returned at all. */
+	none
+
+}
