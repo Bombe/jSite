76c0336dba1b21104b99cd1be7ebc9930d39d8a3
David ‘Bombe’ Roden <bombe@freenetproject.org>
2012-03-17 22:37:59 +0100

    Don’t insert a redirect if force-insert is set.

--- src/de/todesbaum/jsite/application/ProjectInserter.java
+++ src/de/todesbaum/jsite/application/ProjectInserter.java
@@ -282,7 +282,7 @@ public class ProjectInserter implements FileScannerListener, Runnable {
 		if (fileOption.isInsert()) {
 			fileOption.setCurrentHash(file.getHash());
 			/* check if file was modified. */
-			if (file.getHash().equals(fileOption.getLastInsertHash())) {
+			if (!fileOption.isForceInsert() && file.getHash().equals(fileOption.getLastInsertHash())) {
 				/* only insert a redirect. */
 				logger.log(Level.FINE, String.format("Inserting redirect to edition %d for %s.", fileOption.getLastInsertEdition(), filename));
 				return new RedirectFileEntry(filename, fileOption.getMimeType(), "SSK@" + project.getRequestURI() + "/" + project.getPath() + "-" + fileOption.getLastInsertEdition() + "/" + filename);
